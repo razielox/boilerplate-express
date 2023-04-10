626723210fb58dc15f7fc1d974077dcceaa81bf6
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-10 15:46:53 -0500

    15-express-dotenv

--- myApp.js
+++ myApp.js
@@ -1,13 +1,19 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 app.use("/public",express.static(__dirname + '/public'))
 app.get("/", (request, response) => {
     //response.send("Hello Express")
     response.sendFile(__dirname + '/views/index.html')
 })
-app.get("/json", (request, reponse) => {
-    reponse.json({message: "Hello json"})
+app.get("/json", (request, response) => {
+    process.env.MESSAGE_STYLE === 'uppercase'
+
+        ? response.json({message: "HELLO JSON"})
+        : response.json({message: "Hello json"})
+    
 })
+
 
 console.log("Hello World")
 
