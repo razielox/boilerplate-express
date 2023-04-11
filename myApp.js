c6f3890609350e49ad35af6b5de7575fba0fff28
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-11 08:11:38 -0500

    18-router-parameters

--- myApp.js
+++ myApp.js
@@ -1,15 +1,20 @@
 let express = require('express');
 let app = express();
 require('dotenv').config()
+
+//middleware for check all the requests and log variables
 app.use('/', (request, response, next) => {
     console.log(`${request.method} ${request.path} - ${request.ip}`)
     next()
 })
+
+//expose static directory for resources 
 app.use("/public",express.static(__dirname + '/public'))
 app.get("/", (request, response) => {
     //response.send("Hello Express")
     response.sendFile(__dirname + '/views/index.html')
 })
+//environment variable for check before response
 app.get("/json", (request, response) => {
     process.env.MESSAGE_STYLE === 'uppercase'
 
@@ -17,14 +22,18 @@ app.get("/json", (request, response) => {
         : response.json({message: "Hello json"})
     
 })
-
+ 
+//middleware chain
 app.get("/now", (request, response, next) => {
     request.time = new Date().toString()
     next() 
 },
     (request, response) => {
         response.json({time: request.time})
-    
+})
+
+app.get('/:word/echo', (request,response) => {
+    response.json({word: request.params.word})
 })
 
 
